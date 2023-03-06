2164c34bf6239b7a6167ea751e2cbd95de1afaff
Tyler Becks <tylerdbecks@gmail.com>
2023-03-06 13:35:21 -0800

    Add cuisine to los angeles and austin (#37)

---
+++ prisma/migrations/20230305163355_init/migration.sql
@@ -0,0 +1,87 @@
+-- CreateTable
+CREATE TABLE "articles" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMPTZ(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6),
+    "deleted_at" TIMESTAMPTZ(6),
+    "url" VARCHAR NOT NULL,
+    "title" VARCHAR NOT NULL,
+    "image_url" VARCHAR,
+    "description" VARCHAR,
+    "article_source" VARCHAR NOT NULL,
+    "city_id" INTEGER NOT NULL,
+
+    CONSTRAINT "articles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "awards" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMPTZ(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6),
+    "deleted_at" TIMESTAMPTZ(6),
+    "restaurant_id" INTEGER NOT NULL,
+    "award_source" VARCHAR NOT NULL,
+    "award_type" VARCHAR NOT NULL,
+    "year" INTEGER NOT NULL,
+    "url" VARCHAR NOT NULL,
+    "chef" VARCHAR,
+
+    CONSTRAINT "awards_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "cities" (
+    "city" VARCHAR NOT NULL,
+    "state" VARCHAR,
+    "country" VARCHAR NOT NULL,
+    "id" SERIAL NOT NULL,
+
+    CONSTRAINT "cities_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "restaurants" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMPTZ(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6),
+    "deleted_at" TIMESTAMPTZ(6),
+    "name" VARCHAR NOT NULL,
+    "g_place_id" VARCHAR,
+    "city_id" INTEGER NOT NULL,
+
+    CONSTRAINT "restaurants_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_articlestorestaurants" (
+    "created_at" TIMESTAMPTZ(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6),
+    "deleted_at" TIMESTAMPTZ(6),
+    "article_id" INTEGER NOT NULL,
+    "restaurant_id" INTEGER NOT NULL,
+    "id" SERIAL NOT NULL,
+
+    CONSTRAINT "_articlestorestaurants_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "unique_url" ON "articles"("url");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "cities_city_state_country_key" ON "cities"("city", "state", "country");
+
+-- AddForeignKey
+ALTER TABLE "articles" ADD CONSTRAINT "fk_city_id" FOREIGN KEY ("city_id") REFERENCES "cities"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "awards" ADD CONSTRAINT "awards_restaurant_id_fkey" FOREIGN KEY ("restaurant_id") REFERENCES "restaurants"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "restaurants" ADD CONSTRAINT "fk_city_id" FOREIGN KEY ("city_id") REFERENCES "cities"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "_articlestorestaurants" ADD CONSTRAINT "articles_restaurants_article_id_fkey" FOREIGN KEY ("article_id") REFERENCES "articles"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "_articlestorestaurants" ADD CONSTRAINT "articles_restaurants_restaurant_id_fkey" FOREIGN KEY ("restaurant_id") REFERENCES "restaurants"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
---
+++ scripts/add-cuisine-type.ts
@@ -0,0 +1,136 @@
+import { Configuration, OpenAIApi } from 'openai';
+import prisma from '../src/prisma-client.js';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY as string,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const askGPT3 = async (prompt: string) => {
+  if (!configuration.apiKey) {
+    throw new Error(
+      'OpenAI API key not configured, please follow instructions in README.md'
+    );
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt,
+      temperature: 0.3,
+    });
+
+    const firstChoice = completion.data.choices[0].text as string;
+    const cuisine = formatCuisineResult(firstChoice);
+
+    console.log(prompt);
+    console.log('GPT-3 response:');
+    console.log(cuisine);
+
+    return cuisine;
+  } catch (error: any) {
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+    }
+  }
+};
+
+const formatCuisineResult = (cuisine: string) => {
+  cuisine = cuisine.replace('\n', '');
+  // strip last character if it's a period
+  if (cuisine[cuisine.length - 1] === '.') {
+    cuisine = cuisine.slice(0, -1);
+  }
+
+  // script cuisine from the end of the string
+  if (cuisine.slice(-7) === 'cuisine' || cuisine.slice(-7) === 'Cuisine') {
+    cuisine = cuisine.slice(0, -8);
+  }
+
+  return cuisine;
+};
+
+const getCuisine = async (restaurantName: string, city: string) => {
+  const prompt = `Describe the primary cuisine type for ${restaurantName} in ${city} in 5 words or less. Examples: French seafood, upscale Indian, Greek fast casual street food. Don't use the word cuisine.`;
+  return await askGPT3(prompt);
+};
+
+const getCityId = async (cityName: string) => {
+  const city = await prisma.city.findFirst({
+    where: { city: cityName },
+  });
+
+  if (!city) {
+    throw new Error('City not found in database: ' + cityName);
+  }
+
+  return city.id;
+};
+
+const getCuisinesForRestaurantsInCity = async (cityName: string) => {
+  const cityId = await getCityId(cityName);
+
+  const restaurants = await prisma.restaurant.findMany({
+    where: { cityId, cuisine: null },
+  });
+
+  console.log(`${restaurants.length} without cuisine remaining in ${cityName}`);
+
+  for (const restaurant of restaurants) {
+    const cuisine = await getCuisine(restaurant.name, cityName);
+    await prisma.restaurant.update({
+      where: { id: restaurant.id },
+      data: { cuisine: cuisine ?? null },
+    });
+
+    // The openai API has a rate limit of 60 requests per minute
+    await sleep(700);
+  }
+};
+
+const sleep = (ms: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
+
+const CITIES = [
+  { city: 'austin', state: 'tx', country: 'usa' },
+  // { city: 'barcelona', state: null, country: 'spain' },
+  // { city: 'boston', state: 'ma', country: 'usa' },
+  // { city: 'chicago', state: 'il', country: 'usa' },
+  // { city: 'denver', state: 'co', country: 'usa' },
+  // { city: 'houston', state: 'tx', country: 'usa' },
+  // { city: 'las vegas', state: 'nv', country: 'usa' },
+  // { city: 'london', state: null, country: 'united kingdom' },
+  // { city: 'los angeles', state: 'ca', country: 'usa' },
+  // { city: 'mexico city', state: null, country: 'mexico' },
+  // { city: 'miami', state: 'fl', country: 'usa' },
+  // { city: 'munich', state: null, country: 'germany' },
+  // { city: 'nashville', state: 'tn', country: 'usa' },
+  // { city: 'new orleans', state: 'la', country: 'usa' },
+  // { city: 'new york', state: 'ny', country: 'usa' },
+  // { city: 'oaxaca', state: null, country: 'mexico' },
+  // { city: 'paris', state: null, country: 'france' },
+  // { city: 'rome', state: null, country: 'italy' },
+  // { city: 'san diego', state: 'ca', country: 'usa' },
+  // { city: 'san francisco', state: 'ca', country: 'usa' },
+  // { city: 'seattle', state: 'wa', country: 'usa' },
+  // { city: 'tokyo', state: null, country: 'japan' },
+  // { city: 'washington', state: 'dc', country: 'usa' },
+  // { city: 'zurich', state: null, country: 'switzerland' },
+];
+
+const main = async () => {
+  for (const cityMetadata of CITIES) {
+    let { city } = cityMetadata;
+    console.log('====================================================');
+    console.log(`Starting city: ${city}`);
+    getCuisinesForRestaurantsInCity(city);
+  }
+};
+
+main();
