36aeb9c937605199154d0696a6d8b5cc8eec185f
Tyler Becks <tylerdbecks@gmail.com>
2023-04-03 08:26:18 -0700

    Comment out transports

--- src/logger.ts
+++ src/logger.ts
@@ -3,17 +3,17 @@ import { createLogger, format, transports } from 'winston';
 const logger = createLogger({
   level: process.env.LOG ?? 'info',
   format: format.combine(format.colorize(), format.simple()),
-  transports: [
-    new transports.File({
-      filename: 'logs/error.log',
-      level: 'error',
-      format: format.json(),
-    }),
-    new transports.File({
-      filename: 'logs/combined.log',
-      format: format.json(),
-    }),
-  ],
+  // transports: [
+  //   new transports.File({
+  //     filename: 'logs/error.log',
+  //     level: 'error',
+  //     format: format.json(),
+  //   }),
+  //   new transports.File({
+  //     filename: 'logs/combined.log',
+  //     format: format.json(),
+  //   }),
+  // ],
 });
 
 if (process.env.NODE_ENV !== 'production') {
