a63d8282722c3c7455ec7dc97e489279f9c746fb
Tyler Becks <tylerdbecks@gmail.com>
2023-04-10 22:43:57 -0700

    Add sqlite for city cache

--- scripts/utils/city-matcher.ts
+++ scripts/utils/city-matcher.ts
@@ -1,21 +1,28 @@
-import fs from 'fs';
 import path from 'path';
+import { Database, verbose } from 'sqlite3';
 
 import logger from '../../src/logger';
 import prisma from '../../src/prisma-client';
-import cityCache from './city-match-cache.json';
 import askGPT from './open-ai';
 
-const CACHE_FILE = 'city-match-cache.json';
+verbose();
+
+const cacheFilePath = path.join(__dirname, 'city-cache.db');
 
 type City = { id: number; city: string };
 
+type CachedCity = {
+  city: string;
+  cityId: number | null;
+  cityFromDb: string | null;
+};
+
+// Any time we run this script, you must be skeptical of this cache.
+// If a city page was added since the last run, some of these neighborhoods might not be false
 export default class CityNameMatcher {
   citiesFromDB: City[] | undefined;
 
-  // Any time we run this script, you must be skeptical of this cache.
-  // If a city page was added since the last run, some of these neighborhoods might not be false
-  openAIResponseCache = cityCache as Record<string, City | false>;
+  cache = new Database(cacheFilePath);
 
   async loadAllCities() {
     this.citiesFromDB = await prisma.city.findMany({
@@ -26,22 +33,23 @@ export default class CityNameMatcher {
     });
   }
 
-  async matchCityFromDb(cityState: string) {
+  async matchCityFromDb(cityState: string): Promise<City | null> {
     if (!this.citiesFromDB) {
       throw new Error('Must call loadAllCities before calling matchCityFromDb');
     }
 
-    // Check if city previously cached as false, meaning no matches were found in the code below
-    if (this.openAIResponseCache[cityState] === false) {
-      logger.info(`${cityState} cached as false, returning null`);
-      return null;
-    }
+    const cachedCity = await this.getCityFromCache(cityState);
 
-    // Check if city previously cached as cityFromDB, meaning a match was found in the code below
-    if (this.openAIResponseCache[cityState]) {
+    if (cachedCity) {
+      // Check if city previously cached, but with no match in the DB
+      if (cachedCity.cityId === null || cachedCity.cityFromDb === null) {
+        logger.info(`${cityState} cached as false, returning null`);
+        return null;
+      }
+
+      // Check if city previously cached as cityFromDB, meaning a match was found in the code below
       logger.info(`${cityState} cached with value:`);
-      console.log(this.openAIResponseCache[cityState]);
-      return this.openAIResponseCache[cityState] as City;
+      return { id: cachedCity.cityId, city: cachedCity.cityFromDb };
     }
 
     const cachedCityId = await prisma.placeIdCache.findFirst({
@@ -68,8 +76,8 @@ export default class CityNameMatcher {
 
       if (city) {
         logger.info(`Found existing city. Caching and returning ${city.city}`);
-        this.openAIResponseCache[cityState] = city;
-        this.saveCache();
+
+        // this.openAIResponseCache[cityState] = city;
         return city;
       }
     }
@@ -93,8 +101,7 @@ export default class CityNameMatcher {
       logger.info(
         `Found exact city match in DB. Caching and returning ${existingCity.city}`
       );
-      this.openAIResponseCache[cityState] = existingCity;
-      this.saveCache();
+      this.saveToCache(cityState, existingCity.id, existingCity.city);
       return existingCity;
     }
 
@@ -106,23 +113,44 @@ export default class CityNameMatcher {
       const response = await askGPT(prompt);
       if (response.match(/yes/i)) {
         logger.info(`✅ Found city match! Caching and returning ${city.city}`);
-        this.openAIResponseCache[cityState] = city;
-        this.saveCache();
+        this.saveToCache(cityState, city.id, city.city);
         return city;
       }
     }
 
-    this.openAIResponseCache[cityState] = false;
-    this.saveCache();
+    this.saveToCache(cityState, null, null);
 
     return null;
   }
 
-  saveCache() {
-    const filePath = path.join(__dirname, CACHE_FILE);
-    const stringJson = JSON.stringify(this.openAIResponseCache, null, 2);
+  private getCityFromCache(city: string): Promise<CachedCity> {
+    return new Promise((resolve, reject) => {
+      this.cache.get(
+        'select * from cities where city = ?',
+        city,
+        (err, row) => {
+          if (err) {
+            reject(err);
+          }
 
-    fs.writeFileSync(filePath, stringJson);
+          resolve(row as CachedCity);
+        }
+      );
+    });
+  }
+
+  private saveToCache(
+    city: string,
+    cityId: number | null,
+    cityFromDb: string | null
+  ) {
+    const stmt = this.cache.prepare(
+      `INSERT INTO cities (city, cityId, cityFromDb)
+      VALUES (?, ?, ?)`
+    );
+
+    stmt.run(city, cityId, cityFromDb);
+    stmt.finalize();
   }
 }
 
@@ -136,8 +164,3 @@ const getCorrectCityName = (cityName: string) => {
 
   return cityName;
 };
-
-(function () {
-  const cityMatcher = new CityNameMatcher();
-  cityMatcher.saveCache();
-})();
